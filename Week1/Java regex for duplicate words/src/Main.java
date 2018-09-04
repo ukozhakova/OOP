1e94fa5e4a4f6e03d3c61995c44b449ddcc53b7a
Your Name <you@example.com>
2018-09-04 12:41:05 +0600

    Week1

--- Week1/Java regex for duplicate words/src/Main.java
+++ Week1/Java regex for duplicate words/src/Main.java
@@ -1,22 +1,31 @@
-
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        String input = "The the string String string stringing.";
 
         String regex = "\\b(\\w+)(\\s+\\1\\b)+";
-
-        // Use compile(regex) if you want case sensitive.
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
-        Matcher m = p.matcher(input);
-        while (m.find()) {
-            input = input.replaceAll(m.group(), m.group(1));
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+
+            // Prints the modified sentence.
+            System.out.println(input);
         }
 
-        System.out.println(input);
+        in.close();
     }
 }
